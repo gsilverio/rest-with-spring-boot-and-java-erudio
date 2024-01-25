b826c06e531d2b5a7706e2e3dbc2dc8fad14f70b
Guilherme <guilhermegsilverio@gmail.com>
2024-01-25 10:36:54 -0300

    Adding some Migrations

---
+++ backend/src/main/resources/db/migration/V5__Create_Table_Permission.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE IF NOT EXISTS permission (
+                                        id bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
+                                        description varchar(255) DEFAULT NULL,
+                                        PRIMARY KEY (id)
+);
---
+++ backend/src/main/resources/db/migration/V7__Create_Table_Users.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE IF NOT EXISTS users (
+                                    id bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
+                                    user_name varchar(255) DEFAULT NULL,
+                                    full_name varchar(255) DEFAULT NULL,
+                                    password varchar(255) DEFAULT NULL,
+                                    account_non_expired boolean DEFAULT NULL,
+                                    account_non_locked boolean DEFAULT NULL,
+                                    credentials_non_expired boolean DEFAULT NULL,
+                                    enabled boolean DEFAULT NULL,
+                                    PRIMARY KEY (id),
+                                    CONSTRAINT uk_user_name UNIQUE (user_name)
+);
